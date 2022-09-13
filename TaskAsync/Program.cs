dc876a856262739116361bcc8111307d5d69e09a
Jack Lambert <jackwlambert14@gmail.com>
2022-09-13 12:06:38 -0400

    Add project files.

---
+++ TaskAsync/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, Asynchronous programming!");
+
+Task t1 = new(WriteMultipleLines); //no paranthesis just the name; adding paranthesis tells it to execute the method
+
+t1.Start();
+
+
+Console.WriteLine("Done");
+
+t1.Wait();
+
+Console.WriteLine("Finito");
+
+void WriteMultipleLines() {
+
+    Console.WriteLine("Print first line");
+
+    Console.WriteLine("Print second line");
+
+    Console.WriteLine("Print third line");
+}
